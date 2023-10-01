57b0a94c4bb837aac93d221b9d6082248d81644f
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-10-01 12:57:01 +0530

    Add files via upload

---
+++ Nested If.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter a number ";
+	cin>>n;
+	if(n>0){
+		cout<<"This is a Positive integer and";
+		if(n%2==0){
+			cout<<"Even"<<endl;
+		}
+		else{
+			cout<<"odd"<<endl;
+		}
+	}
+	else{
+		cout<<"This is a Negative integer"<<endl;
+		
+	}
+	return 0;
+}
